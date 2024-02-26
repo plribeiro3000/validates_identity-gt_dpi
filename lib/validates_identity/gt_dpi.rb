18db8fad7f313b303cea87c4bca299d702496308
Paulo Ribeiro <plribeiro3000@gmail.com>
2024-02-26 08:17:36 -0300

    chore(*): Initial Commit

---
+++ lib/validates_identity/gt_dpi.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "gt_dpi/version"
+
+module ValidatesIdentity
+  module GtDpi
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
